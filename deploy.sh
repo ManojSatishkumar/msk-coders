11fe7a47839059ab9607ba315b19da619150fb3a
Manoj Satish Kumar <85manojs@gmail.com>
2021-02-20 23:33:45 +0530

    moved to docs

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+# Push frontend
+cd /Users/manoj/Desktop/Projects/msk-coders/msk-coders-frontend;
+sh push.sh;
+
+# Push backend
+cd /Users/manoj/Desktop/Projects/msk-coders/msk-coders-backend;
+sh push.sh
+
+# deploy
+rm -rf /Users/manoj/Desktop/Projects/msk-coders/msk-coders/docs;
+cp -R /Users/manoj/Desktop/Projects/msk-coders/msk-coders-frontend/build /Users/manoj/Desktop/Projects/msk-coders/msk-coders/docs;
+cd /Users/manoj/Desktop/Projects/msk-coders/msk-coders;
+sh push.sh
---
+++ push.sh
@@ -0,0 +1,4 @@
+cd /Users/manoj/Desktop/Projects/msk-coders/msk-coders
+git add .;
+git commit -m "auto push commit";
+git push;
